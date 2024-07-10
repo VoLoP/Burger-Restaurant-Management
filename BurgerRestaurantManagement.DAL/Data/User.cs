60b7ac0dd3077548e3c54136ae313084d19e194c
Simone <bativolpet1@gmail.com>
2024-07-10 14:35:52 +0200

    Added validations and default value to User model for their points
    Added validation for email and phone number in User.cs, ensuring proper formats with error messages. Also, introduced a default value of 0 for the Points property.

--- BurgerRestaurantManagement.DAL/Data/User.cs
+++ BurgerRestaurantManagement.DAL/Data/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,16 @@ namespace BurgerRestaurantManagement.DAL.Data
         public string UserName { get; set; }
         public string Password { get; set; }
         public string FullName { get; set; }
+
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email")]
         public string Email { get; set; }
+
+        [RegularExpression(@"^[0-9]{4,15}$", ErrorMessage = "Invalid phone number")]
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public int Points { get; set; }
+        public int Points { get; set; } = 0;
         public int RoleId { get; set; }
         public Role? Role { get; set; }
     }
