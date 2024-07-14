5fd61407d42eba30748683b8c76c77d4af852c34
Simone <bativolpet1@gmail.com>
2024-07-14 02:25:21 +0200

    Added components and image carousel base
    Added carousel component with carousel base and burger-menu component

--- burger-restaurant-front/src/app/components/app/app.component.ts
+++ burger-restaurant-front/src/app/components/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
+import { CarouselComponent } from "../carousel/carousel.component";
+import { BurgerMenuComponent } from "../burger-menu/burger-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CarouselComponent, BurgerMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
